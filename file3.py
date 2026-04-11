df5ada72a0115992dedb2db12027cf6fe88f6dc5
Ugandhar Reddy <yogipigasian@gmail.com>
2026-04-10 22:06:15 -0400

    Add ApacheHttpClient DynamoDbClient bean to file3.py

--- file3.py
+++ file3.py
@@ -1,1 +1,18 @@
 # Dummy Python file 3
+
+# import software.amazon.awssdk.http.apache.ApacheHttpClient;
+# import java.time.Duration;
+#
+# @Bean
+# public DynamoDbClient dynamoDBClient() throws URISyntaxException {
+#     Region dynamoDBRegion = Region.of(region);
+#     DynamoDbClientBuilder dynamoDbClientBuilder = DynamoDbClient.builder()
+#         .httpClientBuilder(ApacheHttpClient.builder()
+#             .connectionTimeout(Duration.ofSeconds(10))
+#             .socketTimeout(Duration.ofSeconds(30)))
+#         .region(dynamoDBRegion);
+#     if (StringUtils.isNotEmpty(endpoint)) {
+#         dynamoDbClientBuilder.endpointOverride(new URI(endpoint));
+#     }
+#     return dynamoDbClientBuilder.build();
+# }
